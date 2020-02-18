cc536f9142890dc40a255929ba80111b5cb8ef6a
[Ben Wedlund] <[benwed@gmail.com]>
2020-02-18 10:30:11 -0600

    commit prior to drop down menu changes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+    res.render('mcminntest.ejs')
+});
+
+
+
+app.listen(3000, function(req,res){
+    console.log('Server has started');
+});
