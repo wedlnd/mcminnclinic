db0966075e02a045a33f5ea097a947a3725ecf93
Benjamin Wedlund <benwed@gmail.com>
2020-02-27 12:08:28 -0600

    Required helmet for vulnerabilities

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(helmet());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
-    res.render('mcminntest.ejs')
+app.get('/', function(req, res) {
+	res.render('mcminntest.ejs');
 });
 
-app.get('/about', function(req,res){
-    res.render('about');
+app.get('/about', function(req, res) {
+	res.render('about');
 });
 
-app.get('/podcast', function(req,res){
-    res.render('podcast');
+app.get('/podcast', function(req, res) {
+	res.render('podcast');
 });
 
-app.get('/newpatient', function(req,res){
-    res.render('newpatient');
+app.get('/newpatient', function(req, res) {
+	res.render('newpatient');
 });
 
-app.get('/currentpatient', function(req,res){
-    res.render('currentpatient');
+app.get('/currentpatient', function(req, res) {
+	res.render('currentpatient');
 });
 
-app.get('/contact', function(req,res){
-    res.render('contact');
+app.get('/contact', function(req, res) {
+	res.render('contact');
 });
 
-app.listen(3000, function(req,res){
-    console.log('Server has started');
+app.listen(3000, function(req, res) {
+	console.log('Server has started');
 });
